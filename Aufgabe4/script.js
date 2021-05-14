824a671a1f356d403e235418c664a06cbfa07112
Verena <vero_ds@web.de>
2021-05-14 10:13:41 +0200

    TestAufgabe Coca Cola

---
+++ Aufgabe4/script.js
@@ -0,0 +1,69 @@
+"use strict";
+/*function a1(): void {
+    let x: string = "Alles";
+    console.log(x);
+    func1();
+    func2();
+    console.log("Logo!");
+}
+
+a1();
+
+function func1(): void {
+    console.log("Klar?");
+}
+
+function func2(): void {
+    console.log("Nesli hat Spaß");
+}
+
+// Aufgabe 2
+
+function a2(): void {
+    debugger; let i: number = 9;
+
+    do {
+        console.log(i);
+        i = i - 1;
+    } while ( i > 0);
+}
+
+a2();
+
+//Aufgabe 4
+
+let x: string = "Hallo";
+console.log(x);
+func1(x);
+console.log(x);
+func2();
+func3();
+console.log(x);
+
+function func1(y: string): void {
+    y = "Bla";
+    console.log(y);
+}
+
+function func2(): void {
+    let x: string = "Blubb";
+    console.log(x);
+}
+
+function func3(): void {
+    x = "Test";
+}
+
+//Aufgabe 5
+
+var x: number = 5;
+var y: number = 2;
+var z: number = x * y;
+console.log(z);
+
+
+console.log(Math.max(0, 150, 30, 20, -8, -200));
+
+Array.from({length: 100}, (_, x ) => console.log(x + 1 ));*/
+console.log(Math.random());
+//# sourceMappingURL=script.js.map
